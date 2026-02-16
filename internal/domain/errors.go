8369a533e85f31b9c095c1e1981345301639cc59
Werick Nalyson <wericknalyson@gmail.com>
2026-02-16 19:59:17 -0300

    feat(account): create account service

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,6 +3,10 @@ package domain
 import "errors"
 
 var (
-	// ErrAccountNotFound is returned when an account is not found
-	ErrAccountNotFound = errors.New("account not found")
+	ErrAccountNotFound    = errors.New("account not found")
+	ErrDuplicatedAPIKey   = errors.New("api key already exists")
+	ErrInvoiceNotFound    = errors.New("invoice not found")
+	ErrUnauthorizedAccess = errors.New("unauthorized access")
+	ErrInvalidAmount      = errors.New("invalid amount")
+	ErrInvalidStatus      = errors.New("invalid status")
 )
